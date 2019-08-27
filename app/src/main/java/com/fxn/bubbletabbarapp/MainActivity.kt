b45baa788be58be7719a878a079fd50f97680134
Akshay Sharma <fxn769@gmail.com>
2019-08-27 21:46:50 +0530

    Sample Updated

--- app/src/main/java/com/fxn/bubbletabbarapp/MainActivity.kt
+++ app/src/main/java/com/fxn/bubbletabbarapp/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.fxn.bubbletabbarapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.fxn.OnBubbleClickListner
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bubbleTabBar.addBubbLeListner(object : OnBubbleClickListner {
+            override fun onBubbleClick(id: Int) {
+                when (id) {
+                    R.id.home -> Log.e("hello", "Home")
+                    R.id.log -> Log.e("hello", "logger")
+                    R.id.doc -> Log.e("hello", "documents")
+                    R.id.setting -> Log.e("hello", "setting")
+                }
+            }
+        })
     }
 }
